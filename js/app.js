4622412cc4792a1d69027d30ee1cd125fcf6ebe2
natata7 <natata7@gmail.com>
2021-02-07 19:13:02 +0200

    feat: add counter

--- js/app.js
+++ js/app.js
@@ -5,6 +5,11 @@ var clearButton = document.querySelector("button.clear");
 var showTipsButton = document.querySelector("button.showTips");
 var closeTipsButton = document.querySelector("a.closeTips");
 var overlay = document.querySelector("#overlay");
+var allTasks = document.getElementById('All')
+var activeTasks = document.getElementById('Active')
+var completedTasks = document.getElementById('Completed')
+var tasks = [];
+var id = 1;
 
 function loadTodos() {
     var data = localStorage.getItem("todo");
@@ -18,12 +23,14 @@ function loadTodos() {
 }
 loadTodos();
 
-function createTodo() {
+function renderListItem(item) {
+    console.log(item);
     var li = document.createElement("li");
     var textSpan = document.createElement("span");
     textSpan.classList.add("todo-text");
-    var newTodo = input.value;
-    textSpan.append(newTodo);
+
+    textSpan.dataset.indexNumber = item.id;
+    textSpan.append(item.title);
 
     var deleteBtn = document.createElement("span");
     deleteBtn.classList.add("todo-close");
@@ -31,18 +38,44 @@ function createTodo() {
     deleteBtn.appendChild(icon);
 
     list.appendChild(li).append(textSpan, deleteBtn);
-    input.value = "";
     listenDeleteTodo(deleteBtn);
+    counter();
+}
+
+const renderCompletedTodos = () => {
+
+      tasks.filter(task => task.status == 'inProgress').forEach(task => {
+        renderTodo(task, true);
+        counter();
+      });
+}
+
+function createTodo() {
+    addItem();
+    renderListItem(tasks[tasks.length-1]);
+}
+
+function addItem() {
+    var item = {
+        'id': id,
+        'title':input.value,
+        'priority':'normal',
+        'status': 'inProgress'
+    };
+    
+    tasks.push(item);
+    input.value = "";
+    id++;
 }
 
 function listenDeleteTodo(listItem) {
     listItem.addEventListener("click", (event) => {
+        tasks.splice( tasks[listItem.dataset.indexNumber], 1 );
         listItem.parentElement.remove();
         event.stopPropagation();
+        counter();
     });
 }
-
-
 
 input.addEventListener('keydown', function (event) {
     if (event.keyCode != 13) {
@@ -51,17 +84,35 @@ input.addEventListener('keydown', function (event) {
     createTodo();
 });
 
-// Add a "checked" symbol when clicking on a list item
 list.addEventListener('click', function(event) {
   if (event.target.tagName === 'LI') {
+    var status = tasks[(event.target.childNodes[0].dataset.indexNumber) - 1].status;
+    console.log(tasks[(event.target.childNodes[0].dataset.indexNumber) - 1].status);
+    if (status == 'complete') {
+        tasks[event.target.childNodes[0].dataset.indexNumber - 1].status = 'inProgress';
+    } else {
+        tasks[event.target.childNodes[0].dataset.indexNumber - 1].status = 'complete';
+    }
     event.target.classList.toggle('checked');
+    counter();
   }
 }, false);
 
 saveButton.addEventListener("click", () => {
     localStorage.setItem("todo", list.innerHTML);
 });
+
 clearButton.addEventListener("click", () => {
     list.innerHTML = "";
+    tasks = [];
+    counter();
     localStorage.removeItem('todo', list.innerHTML);
 });
+
+function counter(){
+    const itemsCounter = tasks.filter(task => task.status == 'inProgress');
+    const count = document.getElementById('todosLeft');
+
+ count.innerText = `Активные задачи ${itemsCounter.length}`
+
+}
