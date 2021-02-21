881b0dc972542114b6f671862c5b3631c19a23ff
natata7 <natata7@gmail.com>
2021-02-22 00:55:48 +0200

    fix: storing information about tasks

--- js/app.js
+++ js/app.js
@@ -1,118 +1,120 @@
-var list = document.querySelector(".todo-list");
+var list = document.querySelector('.todo-list');
 var input = document.querySelector('#todo_app input');
-var saveButton = document.querySelector("button.save");
-var clearButton = document.querySelector("button.clear");
-var showTipsButton = document.querySelector("button.showTips");
-var closeTipsButton = document.querySelector("a.closeTips");
-var overlay = document.querySelector("#overlay");
-var allTasks = document.getElementById('All')
-var activeTasks = document.getElementById('Active')
-var completedTasks = document.getElementById('Completed')
+var saveButton = document.querySelector('button.save');
+var clearButton = document.querySelector('button.clear');
 var tasks = [];
 var id = 1;
 
 function loadTodos() {
-    var data = localStorage.getItem("todo");
-    if (data) {
-        list.innerHTML = data;
-    }
-    var deleteButtons = document.querySelectorAll("span.todo-trash");
-    for (var button of deleteButtons) {
-        listenDeleteTodo(button);
-    }
+	var data = localStorage.getItem('todo');
+	if (data) {
+		list.innerHTML = data;
+	}
+	var deleteButtons = document.querySelectorAll('span.todo-trash');
+	for (var button of deleteButtons) {
+		listenDeleteTodo(button);
+	}
 }
 loadTodos();
 
 function renderListItem(item) {
-    console.log(item);
-    var li = document.createElement("li");
-    var textSpan = document.createElement("span");
-    textSpan.classList.add("todo-text");
+	var li = document.createElement('li');
+	var textSpan = document.createElement('span');
+	textSpan.classList.add('todo-text');
 
-    textSpan.dataset.indexNumber = item.id;
-    textSpan.append(item.title);
+	textSpan.dataset.indexNumber = item.id;
+	textSpan.append(item.title);
 
-    var deleteBtn = document.createElement("span");
-    deleteBtn.classList.add("todo-close");
-    var icon = document.createTextNode("\u00D7");;
-    deleteBtn.appendChild(icon);
+	var deleteBtn = document.createElement('span');
+	deleteBtn.classList.add('todo-close');
+	var icon = document.createTextNode('\u00D7');
+	deleteBtn.appendChild(icon);
 
-    list.appendChild(li).append(textSpan, deleteBtn);
-    listenDeleteTodo(deleteBtn);
-    counter();
+	list.appendChild(li).append(textSpan, deleteBtn);
+	listenDeleteTodo(deleteBtn);
+	counter();
 }
 
 const renderCompletedTodos = () => {
 
-      tasks.filter(task => task.status == 'inProgress').forEach(task => {
-        renderTodo(task, true);
-        counter();
-      });
-}
+	tasks.filter(task => task.status == 'inProgress').forEach(task => {
+		renderListItem(task, true);
+		counter();
+	});
+};
 
 function createTodo() {
-    addItem();
-    renderListItem(tasks[tasks.length-1]);
+	addItem();
+	renderListItem(tasks[tasks.length-1]);
 }
 
 function addItem() {
-    var item = {
-        'id': id,
-        'title':input.value,
-        'priority':'normal',
-        'status': 'inProgress'
-    };
+	var item = {
+		'id': id,
+		'title':input.value,
+		'priority':'normal',
+		'status': 'inProgress'
+	};
     
-    tasks.push(item);
-    input.value = "";
-    id++;
+	tasks.push(item);
+	input.value = '';
+	id++;
 }
 
 function listenDeleteTodo(listItem) {
-    listItem.addEventListener("click", (event) => {
-        tasks.splice( tasks[listItem.dataset.indexNumber], 1 );
-        listItem.parentElement.remove();
-        event.stopPropagation();
-        counter();
-    });
+	listItem.addEventListener('click', (event) => {
+		//tasks.splice( tasks[listItem.dataset.indexNumber], 1 );
+		listItem.parentElement.remove();
+		event.stopPropagation();
+		counter();
+	});
 }
 
 input.addEventListener('keydown', function (event) {
-    if (event.keyCode != 13) {
-        return; 
-    }
-    createTodo();
+	if (event.keyCode != 13) {
+		return; 
+	}
+	createTodo();
 });
 
 list.addEventListener('click', function(event) {
-  if (event.target.tagName === 'LI') {
-    var status = tasks[(event.target.childNodes[0].dataset.indexNumber) - 1].status;
-    console.log(tasks[(event.target.childNodes[0].dataset.indexNumber) - 1].status);
-    if (status == 'complete') {
-        tasks[event.target.childNodes[0].dataset.indexNumber - 1].status = 'inProgress';
-    } else {
-        tasks[event.target.childNodes[0].dataset.indexNumber - 1].status = 'complete';
-    }
-    event.target.classList.toggle('checked');
-    counter();
-  }
+	if (event.target.tagName === 'LI') {
+		var searchId = (event.target.childNodes[0]).dataset.indexNumber;
+		var taskId;
+		for(var i = 0; i < tasks.length; i++) {
+			if(tasks[i].id == searchId) {
+				taskId = tasks[i];
+				break;
+			}
+		}
+		console.log(taskId);
+
+		var status = tasks[((event.target.childNodes[0]).dataset.indexNumber) - 1].status;
+		console.log(tasks[(event.target.childNodes[0].dataset.indexNumber) - 1].status);
+		if (status == 'complete') {
+			tasks[event.target.childNodes[0].dataset.indexNumber - 1].status = 'inProgress';
+		} else {
+			tasks[event.target.childNodes[0].dataset.indexNumber - 1].status = 'complete';
+		}
+		event.target.classList.toggle('checked');
+		counter();
+	}
 }, false);
 
-saveButton.addEventListener("click", () => {
-    localStorage.setItem("todo", list.innerHTML);
+saveButton.addEventListener('click', () => {
+	localStorage.setItem('todo', list.innerHTML);
 });
 
-clearButton.addEventListener("click", () => {
-    list.innerHTML = "";
-    tasks = [];
-    counter();
-    localStorage.removeItem('todo', list.innerHTML);
+clearButton.addEventListener('click', () => {
+	list.innerHTML = '';
+	counter();
+	localStorage.removeItem('todo', list.innerHTML);
 });
 
 function counter(){
-    const itemsCounter = tasks.filter(task => task.status == 'inProgress');
-    const count = document.getElementById('todosLeft');
+	const itemsCounter = tasks.filter(task => task.status == 'inProgress');
+	const count = document.getElementById('todosLeft');
 
- count.innerText = `Активные задачи ${itemsCounter.length}`
+	count.innerText = `Активные задачи ${itemsCounter.length}`;
 
 }
